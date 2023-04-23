780fc0e9963d89abf57e26e2e43cac71759f5151
Michal <michal.lester@fourthwall.com>
2023-04-23 08:00:29 +0200

    bubble sort

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        const middle = Math.floor((low+high)/2);
+        const v = haystack[middle];
+
+        if (v === needle) {
+            return true;
+        } else if (needle > v) {
+            low = middle + 1;
+        } else {
+            high = middle;
+        }
+    } while (low < high);
+
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+	const jmpAmpunt = Math.floor(Math.sqrt(breaks.length));
+
+	let i = jmpAmpunt;
+	for (; i < breaks.length; i += jmpAmpunt) {
+		if (breaks[i]) {
+			break;
+		}
+	}
+	i -= jmpAmpunt
+
+	for (let j = 0; j< breaks.length; j++, i++) {
+		if (breaks[i]) {
+			return i
+		}
+	}
+
+	return -1
 
 }
